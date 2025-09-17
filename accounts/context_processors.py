9fe5e270f0d64a0eb5d86ec510e7a0c9381fc24a
Eternal-Lost <a972461825@icloud.com>
2025-09-16 20:02:23 -0400

    version 2.2

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -5,9 +5,9 @@ def subscription_tier(request):
     if request.user.is_authenticated:
         total_spent = Order.objects.filter(user=request.user).aggregate(total=Sum('total'))['total'] or 0
 
-        if total_spent >= 50:
+        if total_spent >= 30:
             tier = "Premium"
-        elif total_spent >= 30:
+        elif total_spent >= 15:
             tier = "Medium"
         else:
             tier = "Basic"
